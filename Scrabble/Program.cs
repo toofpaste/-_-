8876fd4e0c8714a58b6035d499f24b87e6f0b2ad
Joe Barnes and Michael Larragueta <joe@>
2019-04-25 09:52:26 -0700

    Add everything and make it work. joe is mean

---
+++ Scrabble/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Scrabble
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter a word :");
+            string userWord = Console.ReadLine();
+
+
+            Letters mikeIsABitch = new Letters(userWord);
+
+            Console.WriteLine("Score: " + mikeIsABitch.GetScore(mikeIsABitch.GetWord()));
+        }
+    }
+}
---
+++ Scrabble/Scrabble.Tests/ModelTests/LettersTests.cs
@@ -0,0 +1,16 @@
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using ProjectName;
+
+//namespace Scrabble.Tests
+//{
+//    [TestClass]
+//    public class LeapYearTest
+//    {
+//        [TestMethod]
+//        public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+//        {
+//            // any necessary logic to prep for test; instantiating new classes, etc.
+//            Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+//        }
+//    }
+//}
